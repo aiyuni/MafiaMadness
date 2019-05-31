6fb441fb6e408e7120337a66de44b654dadcc08d
Aiyuni <perry0406@hotmai.com>
2019-05-30 18:46:47 -0700

    first commit

---
+++ frontend/src/ReactApp.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './ReactApp.css';
+
+function App() {
+  return (
+    <div className="App">
+      hi oscar
+    </div>
+  );
+}
+
+export default App;
